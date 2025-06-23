2e9781f7860ed7aea6232681d7a517c2cef602ac
vidhya.m <vidhyam.stackup@gmail.com>
2025-06-23 12:32:16 +0530

    the backend

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    credentials: true,           
+   origin: [
+  "http://localhost:5173",
+  "https://frontend-blog-9h18.vercel.app"
+],
+credentials: true,           
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
   })
