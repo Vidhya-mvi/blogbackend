2c7bf21b24dbe97dcb529addc1c923a5e9a5fb5b
vidhya.m <vidhyam.stackup@gmail.com>
2025-10-20 01:30:13 +0530

    the token

--- controllers/authController.js
+++ controllers/authController.js
@@ -146,7 +146,7 @@ const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: isProduction,
+         secure: true,   
         sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
