71a3c673ebb419807af0bb27f9245ab4ea4a94fe
vidhya.m <vidhyam.stackup@gmail.com>
2025-10-20 08:29:31 +0530

    the update

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use(
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization","cache-control",
+      "x-requested-with",
+      "pragma"],
   })
 );
 
